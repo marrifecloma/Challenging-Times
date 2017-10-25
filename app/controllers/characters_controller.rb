ac834be4b431df44a525ed8972004c8fad36981f
Marrife Cloma <marrifecloma@gmail.com>
2017-10-25 14:36:55 +0000

    Added to detail view, added link back to show view

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -2,4 +2,8 @@ class CharactersController < ApplicationController
   def show
     @characters = Character.all
   end
+
+  def detail
+    @character = Character.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get '/lizard/:number', to: 'pages#lizards', as: 'lizards', number: /\d+/
 
   get '/characters', to: 'characters#show', as: 'show'
+
+  get '/characters/:id', to: 'characters#detail', as: 'detail', id: /\d+/
 end
